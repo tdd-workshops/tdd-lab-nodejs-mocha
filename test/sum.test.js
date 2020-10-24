fb7c6cdf96ae86539d5e263ff919cd6612433186
Michael Cheng <michael_cheng@tech.gov.sg>
2020-10-24 18:22:30 +0800

    Fix ESLint issues.

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import sum from '../src/sum'
 
-it( '1 + 2 should be 3', function() {
-	expect( sum( 1, 2 ) ).to.equal( 3 )
-} )
+it('1 + 2 should be 3', function () {
+  expect(sum(1, 2)).to.equal(3)
+})
